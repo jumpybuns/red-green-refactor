84d8bc108c90540bfb81a9244888a9977f3b73d3
Jumpybuns <ethanpiercepresents@gmail.com>
2020-12-04 11:18:56 -0800

    refactored name

--- src/name.test.js
+++ src/name.test.js
@@ -1,8 +1,9 @@
 const { getName } = require('./name');
 
 describe('getName function', () => {
+    const spot = { name: 'spot', age: 5, weight: '20 lbs' };
+
     it('returns the name ya dingus', () => {
-        const spot = { name: 'spot', age: 5, weight: '20 lbs' };
         const name = getName(spot);
 
         expect(name)
